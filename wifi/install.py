c93c9ced0146bd3a678cbd5a59cbc16b5b394ed7
emanuele <emanuele.rapisarda.1997@gmail.com>
2024-02-13 18:47:46 +0100

    fixes

--- wifi/install.py
+++ wifi/install.py
@@ -2,48 +2,71 @@ import os
 import shutil
 import subprocess
 
+# Constants
+HOTSPOT_NAME = "IPS"
+HOTSPOT_PASSWORD = "password"
+SERVICE_NAME = "hotspot.service"
+APP_DIRECTORY = "config_wifi_page"
+ETC_APP_DIRECTORY = "/etc/config_wifi_page"
+REQUIREMENTS_FILE = "requirements.txt"
 
-def setup():
-    # Check if the user is root
+
+def check_root():
+    """Check if the script is being run as root."""
     if os.geteuid() != 0:
         print("Please run as root (sudo).")
-        exit()
+        exit(1)
 
-    # Check if nmcli exists
+
+def check_nmcli():
+    """Check if nmcli is installed."""
     if not shutil.which('nmcli'):
         print("nmcli not found. Please install NetworkManager.")
-        exit()
+        exit(1)
 
-    # Parameters for the Raspberry Pi hotspot
-    hotspot_name = "IPS"
-    hotspot_password = "password"
 
-    # Create hotspot
+def install_requirements():
+    """Install Python dependencies from requirements.txt."""
+    requirements_path = os.path.join(os.path.dirname(__file__), REQUIREMENTS_FILE)
+    if os.path.exists(requirements_path):
+        subprocess.run(['pip', 'install', '-r', requirements_path], check=True)
+    else:
+        print(f"Requirements file '{REQUIREMENTS_FILE}' not found.")
+        exit(1)
+
+
+def setup_hotspot():
+    """Create the Wi-Fi hotspot and modify the connection to autoconnect."""
     create_hotspot = subprocess.run(
-        ['nmcli', 'device', 'wifi', 'hotspot', 'ssid', hotspot_name, 'con-name', hotspot_name, 'password',
-         hotspot_password,
-         'ifname', 'wlan0'],
-        capture_output=True,
-        text=True
-    )
+        ['nmcli', 'device', 'wifi', 'hotspot', 'ssid', HOTSPOT_NAME, 'con-name', HOTSPOT_NAME, 'password',
+         HOTSPOT_PASSWORD, 'ifname', 'wlan0'], capture_output=True, text=True)
     if create_hotspot.returncode != 0:
         print("Error creating hotspot:", create_hotspot.stderr)
-        exit()
+        exit(1)
 
-    # Configure hotspot to autorun at boot
-
-    modify_connection = subprocess.run(
-        ['nmcli', 'connection', 'modify', hotspot_name, 'connection.autoconnect', 'yes'],
-        capture_output=True,
-        text=True
-    )
+    modify_connection = subprocess.run(['nmcli', 'connection', 'modify', HOTSPOT_NAME, 'connection.autoconnect', 'yes'],
+                                       capture_output=True, text=True)
     if modify_connection.returncode != 0:
         print("Error modifying connection:", modify_connection.stderr)
-        exit()
+        exit(1)
 
-    # Autorun wifi configuration page at boot
-    this_directory = os.path.dirname(os.path.realpath(__file__))
-    service_file = "/etc/systemd/system/hotspot.service"
+    print(f"Hotspot \"{HOTSPOT_NAME}\" created successfully")
+
+
+def setup_autorun():
+    """Setup autorun for the Wi-Fi configuration page."""
+    # Copy the application folder to /etc
+    full_app_dir = os.path.join(os.path.dirname(__file__), APP_DIRECTORY)
+    if not os.path.exists(ETC_APP_DIRECTORY):
+        shutil.copytree(full_app_dir, ETC_APP_DIRECTORY)
+        print("Application folder copied to /etc")
+    else:
+        print("Application folder already exists in /etc")
+
+    service_file = f"/etc/systemd/system/{SERVICE_NAME}"
+
+    # Stop service if it's already running
+    subprocess.run(['systemctl', 'stop', SERVICE_NAME], capture_output=True, text=True)
 
     with open(service_file, 'w') as f:
         f.write(f"[Unit]\n"
@@ -52,7 +75,7 @@ def setup():
                 f"Requires=network.target\n\n"
                 f"[Service]\n"
                 f"Type=idle\n"
-                f"ExecStart=/usr/bin/python3 {this_directory}/config_wifi_page/app.py\n"
+                f"ExecStart=/usr/bin/python3 {ETC_APP_DIRECTORY}/app.py\n"
                 f"Restart=always\n"
                 f"RestartSec=10\n"
                 f"User=root\n\n"
@@ -63,18 +86,33 @@ def setup():
     daemon_reload = subprocess.run(['systemctl', 'daemon-reload'], capture_output=True, text=True)
     if daemon_reload.returncode != 0:
         print("Error reloading systemd daemon:", daemon_reload.stderr)
-        exit()
+        exit(1)
 
-    enable_service = subprocess.run(['systemctl', 'enable', 'hotspot.service'], capture_output=True, text=True)
+    enable_service = subprocess.run(['systemctl', 'enable', SERVICE_NAME], capture_output=True, text=True)
     if enable_service.returncode != 0:
         print("Error enabling hotspot service:", enable_service.stderr)
-        exit()
-    start_service = subprocess.run(['systemctl', 'start', 'hotspot.service'], capture_output=True, text=True)
+        exit(1)
+
+    start_service = subprocess.run(['systemctl', 'start', SERVICE_NAME], capture_output=True, text=True)
     if start_service.returncode != 0:
         print("Error starting hotspot service:", start_service.stderr)
-        exit()
+        exit(1)
 
-    print("Setup completed successfully!")
+    print(f"Setup service \"{SERVICE_NAME}\" completed successfully!")
+
+
+def setup():
+    """Main setup function."""
+    try:
+        check_root()
+        check_nmcli()
+        install_requirements()
+        setup_hotspot()
+        setup_autorun()
+        print("Setup completed successfully!")
+    except Exception as e:
+        print(f"An error occurred during setup: {str(e)}")
+        exit(1)
 
 
 if __name__ == '__main__':
