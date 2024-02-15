4b068f535b55c08a1e46348630aa4a4738a8e835
emanuele <emanuele.rapisarda.1997@gmail.com>
2024-02-15 11:50:34 +0100

    fix-installer

--- box/setup.py
+++ box/setup.py
@@ -1,8 +1,37 @@
 import os
+import subprocess
 
 import sys
 
-import setup_lib
+
+def install_prereqs():
+    """
+    Install necessary packages and libraries.
+    """
+    subprocess.run(['sudo', 'apt-get', 'install', 'python3-pip', 'libglib2.0-dev'])
+    subprocess.run(['sudo', 'apt', 'update'])
+    subprocess.run(['sudo', 'apt', 'install', 'python3', 'python3-rpi.gpio', 'python3-pip', '-y'])
+
+    print("Installing Flask web server...")
+    subprocess.run(['sudo', '.venv/bin/pip', 'install', 'flask'])
+    print("Installing library...")
+    subprocess.run(['sudo', 'apt', 'install', 'gcc'])
+    subprocess.run(['sudo', '.venv/bin/pip', 'install', 'numpy'])
+    subprocess.run(['sudo', '.venv/bin/pip', 'install', 'flask_sslify'])
+    subprocess.run(
+        ['sudo', '.venv/bin/pip', 'install', 'keyboard', 'melopero_lsm9ds1', 'requests', 'wifi', 'bluepy', 'GPIO',
+         'RPi.GPIO', 'pyOpenSSL', 'pandas', 'datetime', 'pillow', 'scipy', 'luma.core', 'luma.oled', 'matplotlib'])
+
+
+def copy_configs():
+    """
+    Copy configuration files to appropriate directories.
+    """
+    subprocess.run(['sudo', 'rm', '-r', '/lib/ips_project'])
+    subprocess.run(['sudo', 'rm', '-r', '/etc/ips_project'])
+    subprocess.run(['sudo', 'mkdir', '/usr/lib/ips_project'])
+    subprocess.run(['sudo', 'mkdir', '/etc/ips_project'])
+    subprocess.run(['sudo', 'cp', '-a', '*', '/usr/lib/ips_project/'])
 
 
 def main():
@@ -19,8 +48,8 @@ def main():
     install_ans = input("Sei pronto per eseguire il commit delle modifiche al sistema? [y/N]: ")
 
     if install_ans.lower() == 'y':
-        setup_lib.install_prereqs()
-        setup_lib.copy_configs()
+        install_prereqs()
+        copy_configs()
     else:
         print("\n" * 2 + "=" * 51)
         print("-" * 51)
