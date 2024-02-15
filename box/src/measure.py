c1b2372d861c54764dcfb3fce117807a6e9a86bb
emanuele <emanuele.rapisarda.1997@gmail.com>
2024-02-15 13:02:27 +0100

    fix-src

--- box/src/measure.py
+++ box/src/measure.py
@@ -31,7 +31,7 @@ def set_sensor_odr(sensor, odr):
 
 def singola(sensor):
     try:
-        A = np.genfromtxt('config/config/matrix.txt', delimiter='\t')
+        A = np.genfromtxt('confimatrix.txt', delimiter='\t')
         b = np.genfromtxt('config/bias.txt', delimiter='\t')
         date = np.zeros((0, 3))
         mag_measurements = sensor.get_mag()
